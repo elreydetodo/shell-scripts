0fdc5759116c62ec47a16e3ce94d97859b4f49e1
Martin Meyer <elreydetodo@gmail.com>
2024-11-22 19:14:14 -0500

    Add mysql-client to PATH

--- exports-paths.sh
+++ exports-paths.sh
@@ -1,6 +1,7 @@
 export PATH="/opt/homebrew/opt/gawk/libexec/gnubin:$PATH"
 export PATH="/opt/homebrew/opt/libxslt/bin:$PATH"
 #export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+export PATH="/opt/homebrew/opt/mysql-client@8.4/bin:$PATH"
 export PATH="/opt/homebrew/opt/python@3.11/bin:$PATH"
 export PATH="/opt/homebrew/opt/sbin:$PATH"
 export PATH="/opt/homebrew/opt/bin:$PATH"
