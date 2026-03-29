4e659395922254b81caa765653e4ec980dc98037
Martin Meyer <elreydetodo@gmail.com>
2026-03-29 13:43:56 -0400

    Cleanup path exports
    - Use an array to specify the targets
    - Update to reflect Apple silicon paths for Homebrew
    - Use a native glob expansion to only add paths that really exist
    - Use a typedef instruction to force the path list to be unique

--- exports-paths.sh
+++ exports-paths.sh
@@ -1,12 +1,30 @@
-export PATH="/opt/homebrew/opt/gawk/libexec/gnubin:$PATH"
-export PATH="/opt/homebrew/opt/libxslt/bin:$PATH"
-#export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
-export PATH="/opt/homebrew/opt/mysql-client@8.4/bin:$PATH"
-export PATH="/opt/homebrew/opt/python@3.11/bin:$PATH"
-export PATH="/opt/homebrew/opt/sbin:$PATH"
-export PATH="/opt/homebrew/opt/bin:$PATH"
-export PATH="$HOME/.bin:$PATH"
+#!/bin/bash
 
-if [[ -e ~/.zsh-local-exports.sh ]]; then
+# 1. Define your desired paths in a standard array
+local HOMEBREW_PATHS=(
+    "${HOMEBREW_PREFIX}/opt/gawk/libexec/gnubin"
+    "${HOMEBREW_PREFIX}/opt/libxslt/bin"
+    "${HOMEBREW_PREFIX}/opt/mariadb-connector-c/bin"
+    "${HOMEBREW_PREFIX}/opt/mysql-client@8.4/bin"
+    "${HOMEBREW_PREFIX}/sbin"
+    "${HOMEBREW_PREFIX}/bin"
+    "${HOME}/.bin"
+)
+
+# 2. Ensure 'path' stays unique
+# `typeset -U` makes sure ${path} only allows unique elements.
+# $path is a ZSH built-in array represenation of ${PATH}.
+typeset -U path
+
+
+# 3. Add them to path only if they exist
+# (N-/) is the magic:
+# N: sets NULL_GLOB (ignores the entry if it doesn't exist)
+# -: follows symbolic links
+# /: matches only directories
+path=( "${HOMEBREW_PATHS[@]}"(N-/) $path )
+
+# 4. Evaluate any system-specific paths.
+if [ -e ~/.zsh-local-exports.sh ]; then
     source ~/.zsh-local-exports.sh
 fi
