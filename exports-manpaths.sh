1e4a653c81be9c3771531a81f648b67ff706ea5d
Martin Meyer <elreydetodo@gmail.com>
2026-03-29 13:04:22 -0400

    Update manpath exports to reflect silicon mac paths for homebrew

--- exports-manpaths.sh
+++ exports-manpaths.sh
@@ -1,6 +1,5 @@
+export MANPATH="${HOMEBREW_PREFIX}/share/man:${MANPATH}"
 export MANPATH="/usr/local/share/man/:$MANPATH"
-export MANPATH="/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH"
-export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
 if [[ -e ~/.zsh-local-manpaths.sh ]]; then
     source ~/.zsh-local-manpaths.sh
