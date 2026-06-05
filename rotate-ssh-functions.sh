15dcbbd4004d622fd4f85951d103b91a85c084cc
Martin Meyer <mmeyer@akamai.com>
2026-06-05 17:57:48 -0400

    Updates to rotate-ssh-functions.sh
    - Make variable names lowercase
    - Change ssh key filename pattern

--- rotate-ssh-functions.sh
+++ rotate-ssh-functions.sh
@@ -1,121 +1,120 @@
 #!/bin/bash
 
 function ssh_agent_add() {
-    local LATEST
-    local MONIKER
-    local PREFIX
-    PREFIX=$1
-    if [ -z "$PREFIX" ]; then
+    local latest
+    local moniker
+    local prefix
+    prefix=$1
+    if [ -z "$prefix" ]; then
         echo "SSH key name prefix is required."
         return
     fi
-    MONIKER=$2
-    if [ -z "$MONIKER" ]; then
+    moniker=$2
+    if [ -z "$moniker" ]; then
         echo "SSH key moniker is required."
         return
     fi
-    LATEST=$(gfind ~/.ssh -name "${PREFIX}-*-${MONIKER}" | grep -v -e .pub -e latest | sort -r | head -n 1 | xargs basename)
-    if [ -z "$LATEST" ]; then
-        echo "No matching SSH key found for ${PREFIX}-*-${MONIKER}"
+    latest=$(gfind ~/.ssh -name "${prefix}_${moniker}_*" | grep -v -e .pub -e latest | sort -r | head -n 1 | xargs basename)
+    if [ -z "$latest" ]; then
+        echo "No matching SSH key found for ${prefix}_${moniker}_*"
         return
     fi
-    echo "Latest SSH identity file is: ${LATEST}"
-    ssh-add --apple-use-keychain ~/.ssh/"${LATEST}"
+    echo "Latest SSH identity file is: ${latest}"
+    ssh-add --apple-use-keychain ~/.ssh/"${latest}"
 }
 
 function ssh_agent_cleanup() {
-    local LATEST
-    local MONIKER
-    local PREFIX
-    PREFIX=$1
-    if [ -z "$PREFIX" ]; then
+    local latest
+    local moniker
+    local prefix
+    prefix=$1
+    if [ -z "$prefix" ]; then
         echo "SSH key name prefix is required."
         return
     fi
-    MONIKER=$2
-    if [ -z "$MONIKER" ]; then
+    moniker=$2
+    if [ -z "$moniker" ]; then
         echo "SSH key moniker is required."
         return
     fi
-    LATEST=$(gfind ~/.ssh -name "${PREFIX}-*-${MONIKER}" | grep -v -e .pub -e latest | sort -r | head -n 1 | xargs basename)
-    if [ -z "$LATEST" ]; then
-        LATEST="foo" # we just need something to prevent bad input to grep below
+    latest=$(gfind ~/.ssh -name "${prefix}_${moniker}_*" | grep -v -e .pub -e latest | sort -r | head -n 1 | xargs basename)
+    if [ -z "$latest" ]; then
+        latest="foo" # we just need something to prevent bad input to grep below
     fi
-    echo "Latest SSH identity file is: ${LATEST}"
-    for IDENT in $(ssh-add -l | \
-        grep "${PREFIX}-.*${MONIKER}" | \
-        grep -v "${LATEST}" | \
+    echo "Latest SSH identity file is: ${latest}"
+    for identity in $(ssh-add -l | \
+        grep "${prefix}_${moniker}_.*" | \
+        grep -v "${latest}" | \
         awk '{print $3}'); do
-        echo "Purgine identity file from ssh-agent: ${IDENT}"
-        ssh-add -d ~/.ssh/"${IDENT}"
+        echo "Purging identity file from ssh-agent: ${identity}"
+        ssh-add -d ~/.ssh/"${identity}"
     done
 }
 
 function ssh_create_key() {
-    local DATE
-    local FILENAME
-    local MONIKER
-    local PREFIX
-    PREFIX=$1
-    if [ -z "$PREFIX" ]; then
+    local date
+    local filename
+    local moniker
+    local prefix
+    prefix=$1
+    if [ -z "$prefix" ]; then
         echo "SSH key name prefix is required."
         return
     fi
-    MONIKER=$2
-    if [ -z "$MONIKER" ]; then
+    moniker=$2
+    if [ -z "$moniker" ]; then
         echo "SSH key moniker is required."
         return
     fi
-    DATE=$(date +%F)
-    FILENAME="${PREFIX}-${DATE}-${MONIKER}"
-    echo "Generating identity file: ~/.ssh/${IDENT}"
-    ssh-keygen -t rsa -b 2048 -C "${FILENAME}" -f ~/.ssh/"${FILENAME}"
+    date=$(date +%F)
+    filename="${prefix}_${moniker}_${date}"
+    echo "Generating identity file: ~/.ssh/${filename}"
+    ssh-keygen -t rsa -b 2048 -C "${filename}" -f ~/.ssh/"${filename}"
 }
 
 function ssh_link_latest() {
-    local FILENAME
-    local LATEST
-    local MONIKER
-    local PREFIX
-    PREFIX=$1
-    if [ -z "$PREFIX" ]; then
+    local filename
+    local latest
+    local moniker
+    local prefix
+    prefix=$1
+    if [ -z "$prefix" ]; then
         echo "SSH key name prefix is required."
         return
     fi
-    MONIKER=$2
-    if [ -z "$MONIKER" ]; then
+    moniker=$2
+    if [ -z "$moniker" ]; then
         echo "SSH key moniker is required."
         return
     fi
-    FILENAME=$(gfind ~/.ssh -name "${PREFIX}-*-${MONIKER}" | grep -v -e .pub -e latest | sort -r | head -n 1)
-    if [ -z "$FILENAME" ]; then
-        echo "No matching SSH key found for ${PREFIX}-*-${MONIKER}"
+    filename=$(gfind ~/.ssh -name "${prefix}_${moniker}_*" | grep -v -e .pub -e latest | sort -r | head -n 1)
+    if [ -z "$filename" ]; then
+        echo "No matching SSH key found for ${prefix}_${moniker}_*"
         return
     fi
-    LATEST="${PREFIX}-latest-${MONIKER}"
-    ln -sf "${FILENAME}" ~/.ssh/"${LATEST}"
-    ln -sf "${FILENAME}.pub" ~/.ssh/"${LATEST}".pub
+    latest="${prefix}_${moniker}_latest"
+    ln -sf "${filename}" ~/.ssh/"${latest}"
+    ln -sf "${filename}.pub" ~/.ssh/"${latest}".pub
 }
 
 ssh_print_fingerprint() {
-    local FILENAME
-    local LATEST
-    local MONIKER
-    local PREFIX
-    PREFIX=$1
-    if [ -z "$PREFIX" ]; then
+    local filename
+    local moniker
+    local prefix
+    prefix=$1
+    if [ -z "$prefix" ]; then
         echo "SSH key name prefix is required."
         return
     fi
-    MONIKER=$2
-    if [ -z "$MONIKER" ]; then
+    moniker=$2
+    if [ -z "$moniker" ]; then
         echo "SSH key moniker is required."
         return
     fi
-    FILENAME=$(gfind ~/.ssh/ -name "${PREFIX}-*-${MONIKER}" | grep -v -e .pub -e latest | sort -r | head -n 1 | xargs basename)
-    if [ -z "$FILENAME" ]; then
-        echo "No matching SSH key found for ${PREFIX}-*-${MONIKER}"
+    filename=$(gfind ~/.ssh/ -name "${prefix}_${moniker}_*" | grep -v -e .pub -e latest | sort -r | head -n 1 | xargs basename)
+    if [ -z "$filename" ]; then
+        echo "No matching SSH key found for ${prefix}_${moniker}_*"
         return
     fi
-    ssh-keygen -l -f ~/.ssh/"${FILENAME}".pub
+    ssh-keygen -l -f ~/.ssh/"${filename}".pub
 }
