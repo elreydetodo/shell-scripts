a592979be04dabc778dac7815d9198e8a606b9f1
Martin Meyer <elreydetodo@gmail.com>
2026-06-15 19:14:34 -0400

    Add alias from vim to nvim, if nvim is installed

--- exports-aliases.sh
+++ exports-aliases.sh
@@ -2,6 +2,11 @@
 alias gg="git grep -n"
 alias gnpg="git --no-pager grep -n"
 
+# Alias vim to nvim if Neovim is installed
+if command -v nvim &> /dev/null; then
+    alias vim="nvim"
+fi
+
 if [[ -e ~/.zsh-local-aliases.sh ]]; then
     source ~/.zsh-local-aliases.sh
 fi
